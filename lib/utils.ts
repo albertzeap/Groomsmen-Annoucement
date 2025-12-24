3097264e3ce1c465d4f6095fc070d12c54bcab6b
Albert Paez <albertzeap@gmail.com>
2025-12-24 00:03:19 -0800

    Add basic functionality to parse env variable for list of groomsmen name

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Groomsman } from "@/data/groomsmen";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function convertEnvToGroomsman() {
+  const groomsmanString = process.env.NEXT_PUBLIC_GROOMSMEN_NAMES;
+  if (!groomsmanString) {
+    console.error("Could not load groomsmen names");
+  }
+  const namesList = groomsmanString?.split(",");
+  const groomsmen = new Map<string, Groomsman>();
+  namesList?.forEach((name) => {
+    let nameParts = name.split(" ");
+    let firstName = nameParts[0];
+    let lastName = nameParts[1];
+    let groomsman = { firstName: firstName, lastName: lastName };
+    groomsmen.set(name, groomsman);
+  });
+
+  return groomsmen;
 }
