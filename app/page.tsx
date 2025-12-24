a6fdcf5b3082d1494b8c23829830d9dbd0410051
Albert Paez <albertzeap@gmail.com>
2025-12-24 00:03:54 -0800

    Add rough implementation of opening form

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
-import { ComponentExample } from "@/components/component-example";
+import { groomsmen } from "@/data/groomsmen";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Example, ExampleWrapper } from "@/components/example";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+} from "@/components/ui/field";
 
 export default function Page() {
   return (
-    <div>
-      <ComponentExample />;
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="text-xl mb-9">
+        Seems like we have some important news for you!
+      </div>
+      <form className="flex flex-col items-center gap-4 w-full max-w-md mx-auto">
+        <Field className="w-full">
+          <FieldLabel htmlFor="firstName">First Name</FieldLabel>
+          <Input id="firstName" type="text" placeholder="Albert" />
+        </Field>
+        <Field className="w-full">
+          <FieldLabel htmlFor="lastName">Last Name</FieldLabel>
+          <Input id="lastName" type="text" placeholder="Paez" />
+          <Button type="submit" variant="ghost">
+            Submit
+          </Button>
+        </Field>
+      </form>
     </div>
   );
 }
-
