ffabaf5da29920bc6eb5f690cc0fa0fb843100f0
Albert Paez <albertzeap@gmail.com>
2025-12-27 00:22:59 -0800

    Fix strokeWidth prop spreading bug

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/components/ui/spinner";
 export function Loader({ message }: { message: string }) {
   return (
     <>
-      <Spinner className="size-10 m-3" />
+      <Spinner className="size-10" />
       <h1 className="text-lg">{message}</h1>
     </>
   );
