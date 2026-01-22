ba1970c0de35ef9d79aafd5c77bd88b0bdd26576
Albert Paez <albertzeap@gmail.com>
2026-01-21 20:58:33 -0800

    Add additional questions

--- data/questions.ts
+++ data/questions.ts
@@ -55,6 +55,53 @@ export const questions: Question[] = [
   },
   {
     id: "3",
+    title: "Emotional Check 🫀",
+    titleDescription:
+      "Scenario: You are 1 point away from winning a pickleball match. Using your spy-like abilities, you successfully return the ball that was coming at you. It's at this moment that the opponent makes a mistake. He hits the ball up high enough for you to slam it. With every fiber of your being, you prepare to hit the grand slam that you see set out before you. Then BOOM! You suddenly feel a sharp pain on your head. You open your eyes to see that your partner has hit you in the head trying to hit the grand slam that was set before you. How does this make you feel?  ",
+    choices: [
+      {
+        id: "A-😵‍💫❓",
+        choice: "I'm confused because I don't even play pickleball",
+      },
+      {
+        id: "B-😡💥",
+        choice: "BBOOOOOYYY HE BETTER RUUUN",
+      },
+      {
+        id: "C-🤝😊",
+        choice:
+          "It's okay. It's the thought that counts. I appreciate his effort and his tenacity.",
+      },
+      {
+        id: "D-🤬😵‍💫",
+        choice:
+          "It's okay everything is fine, it's okay everything is fine, it's okay everything is fine",
+      },
+    ],
+  },
+  {
+    id: "4",
+    title: "Check 📝💸",
+    titleDescription:
+      "Scenario: You are tasked with a spy mission in Rome. You decide to walk the streets to do a quick survey of the surrounding area. While you're looking around, you feel a sudden tap on your shoulder. Naturally you turn around to see who it is. Upon turning and looking, you see an old man who is holding a piece of paper. He then looks at you and asks 'Can you write me a check?' How much do you give him? ",
+    choices: [
+      {
+        id: "A-💸❌",
+        choice: "I write a BIG FAT ZEEERROOO because I DON'T GOT MoNeY",
+      },
+      {
+        id: "B-🫶💰",
+        choice: "I give him everything I own. All my cash is his",
+      },
+      {
+        id: "C-🧮🤔",
+        choice: "$3271.08",
+      },
+      { id: "D-🤓➗", choice: "$[(500 * 2) - (500 * 2 - 777)].00" },
+    ],
+  },
+  {
+    id: "5",
     title: "Preparation Check 💪",
     titleDescription:
       "Scenario: You are about to be tasked with a mission that requires love, joy, peace, patience, kindness, goodness, faithfulness, gentleness, and self control. How do you feel?   ",
